ba3eeb4cc629369ae9302dcdc73b291197fdcab1
Stephanie Martínez <stephanie_martinezh@hotmail.com>
2020-08-22 16:33:23 -0600

    UI terminado

--- examen/src/App.js
+++ examen/src/App.js
@@ -1,36 +1,60 @@
 import React from 'react';
 import './App.css';
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, FormGroup, Button } from 'reactstrap';
 
 function App() {
+  //var fechaLocal = fecha();
   return (
-    /* Nombre y slogan de la página */
     <div className="App">
       <header className="App-header" >
-        <p>Evernote wannabe</p>
+        <p>Born to remember</p>
       </header>
+
       <div className="App-slogan">
         <p>Olvídate del papel y recuerda todo</p>
       </div>
-      <div className="App-rectanguloEtiquetas">
-        <InputGroup>
-          <InputGroupAddon addonType="prepend">
-            <InputGroupText>#</InputGroupText>
-          </InputGroupAddon>
-          <Input placeholder="Etiqutas" />
-        </InputGroup>
-      <br />
+
+      <div className="App-rectanguloMostrar">
+        <p>Aquí irían los apuntes guardados</p>
       </div>
+
+    <div className="App-rectanguloEtiqueta">
+       <InputGroup>
+            <InputGroupAddon addonType="prepend">
+              <InputGroupText>#</InputGroupText>
+            </InputGroupAddon>
+            <Input placeholder="Etiquetas" />
+          </InputGroup>
+        <br />
+      </div>
+
+    <div className="App-rectanguloEscribir">
+      <FormGroup>
+          <Input type="textarea" name="text" id="apunte" />
+      </FormGroup>
     </div>
 
-    /* Rectángulo dónde se miran las etiquetas guardadas 
-    <div className="App">
-        <div className="App-rectanguloEtiquetas">
-          <p>Esto es una prueba</p>
-        </div>
-    </div>*/
+    <div className="App-fecha">
+      <p>Fecha: </p>
+      <div id="resultado"></div>
+    </div>
 
+    <div className="App-botonGuardar">
+      <Button color="primary">Guardar</Button>{' '}
+    </div>
+  </div>
+    
   );
 }
+
+/*function fecha(){
+  var objFecha = new Date();
+  var dia  = objFecha.getDate();
+  var mes  = objFecha.getMonth();
+  var anio = objFecha.getFullYear();
+  var fechaT =  dia+'/'+mes+'/'+anio;
+  fechaT = document.getElementById('resultado');
+};*/
+
 
 export default App;
