e15d2922a33775fa2958ab8236e5e7d699ebec8c
Stephanie Martínez <stephanie_martinezh@hotmail.com>
2020-08-22 14:05:09 -0600

    Primera parte del UI

--- examen/src/App.js
+++ examen/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 function App() {
   return (
+    /* Nombre y slogan de la página */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <p>Evernote wannabe</p>
       </header>
+      <div className="App-slogan">
+        <p>Olvídate del papel y recuerda todo</p>
+      </div>
+      <div className="App-rectanguloEtiquetas">
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>#</InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder="Etiqutas" />
+        </InputGroup>
+      <br />
+      </div>
     </div>
+
+    /* Rectángulo dónde se miran las etiquetas guardadas 
+    <div className="App">
+        <div className="App-rectanguloEtiquetas">
+          <p>Esto es una prueba</p>
+        </div>
+    </div>*/
+
   );
 }
 
